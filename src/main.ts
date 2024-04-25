cd9631f6457a17a7e817248a53a9ef6039e3c645
Brudar Artem Sergeevich <71312371+progagoda@users.noreply.github.com>
2024-04-25 16:20:57 +0300

    try again

--- src/main.ts
+++ src/main.ts
@@ -18,44 +18,17 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
   app.enableCors();
-  await app.listen(port);
 
   // get the swagger json file (if app is running in development mode)
   const serverUrl = `http://dop-sheduler-backend.vercel.app`;
   if (process.env.NODE_ENV === 'development') {
-    // write swagger ui files
     get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
       response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
       console.log(
         `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
       );
     });
-
-    get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
-      console.log(
-        `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
-      );
-    });
-
-    get(
-      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
-      function (response) {
-        response.pipe(
-          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
-        );
-        console.log(
-          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
-        );
-      },
-    );
-
-    get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
-      console.log(
-        `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
-      );
-    });
   }
+  await app.listen(port);
 }
 bootstrap();
